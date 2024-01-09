ca6b781efd4e8e61d8651a534657f7c3a19a3f87
Montags24 <adrianmontagu@hotmail.co.uk>
2024-01-09 21:18:37 +0000

    update db_script.py

--- db_script.py
+++ db_script.py
@@ -326,16 +326,79 @@ def delete_all_entries():
     db.session.commit()
 
 
+def get_user_projects(user_id, org_id):
+    user_project_info = (
+        db.session.query(User.name, Project.name)
+        .join(User_Organisation, User.id == User_Organisation.user_id)
+        .join(
+            ProjectOrganisation,
+            User_Organisation.organisation_id == ProjectOrganisation.organisation_id,
+        )
+        .join(Project, ProjectOrganisation.project_id == Project.id)
+        .filter(User.id == user_id)
+        .first()
+    )
+    if user_project_info:
+        user_name, project_name = user_project_info
+        print(f"{user_name} has got {project_name}")
+    else:
+        print("User not found or not associated with a project")
+
+    get_project_tasks(project_name, org_id)
+
+
+def get_project_tasks(project_name, org_id):
+    # project_tasks_info = (
+    #     db.session.query(Task.name).join(Project, Project.name == project_name).all()
+    # )
+    project_tasks_info = (
+        db.session.query(Task)
+        .join(Project, Task.project_id == Project.id)
+        .filter(Project.name == project_name)
+        .all()
+    )
+    for task in project_tasks_info:
+        print(task.name)
+
+    get_task_progress(project_tasks_info[0].id, org_id)
+    return
+
+
+def get_task_progress(task_id, org_id):
+    task_progress_info = (
+        db.session.query(TaskProgress)
+        .join(Task, Task.id == TaskProgress.task_id)
+        .join(Organisation, Organisation.id == TaskProgress.organisation_id)
+        .filter(Task.id == task_id)
+        .filter(Organisation.id == org_id)
+        .first()
+    )
+
+    if task_progress_info:
+        task_status_id = task_progress_info.task_status_id
+        print(
+            f"Task Status ID for Task {task_id} in Organisation {org_id}: {task_status_id}"
+        )
+    else:
+        print(
+            f"No task progress information found for Task {task_id} in Organisation {org_id}"
+        )
+
+    return task_progress_info
+
+
 if __name__ == "__main__":
     with app.app_context():
         # Run the create_roles function when this script is executed directly
-        delete_all_entries()
-        add_org_heirarchy()
-        add_orgs()
-        add_users()
-        assign_users_to_orgs()
-        create_initial_org_groups()
-        create_project()
-        create_task_status()
-        create_tasks()
-        create_task_progress()
+        # delete_all_entries()
+        # add_org_heirarchy()
+        # add_orgs()
+        # add_users()
+        # assign_users_to_orgs()
+        # create_initial_org_groups()
+        # create_project()
+        # create_task_status()
+        # create_tasks()
+        # create_task_progress()
+
+        get_user_projects(user_id=902, org_id=4774)
