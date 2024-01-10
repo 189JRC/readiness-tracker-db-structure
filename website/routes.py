cc38cac6c3ac110f64a49c1df43d162d00b9db82
John Clarke <john.clarke@dt-squad.com>
2024-01-10 13:10:56 +0000

    started to serialise objects for json output in routes

--- website/routes.py
+++ website/routes.py
@@ -19,3 +19,33 @@ from website.models import (
 def index():
     # This is a vue project that serves the static index file only
     return render_template("index.html")
+
+@app.route("/users", methods=["GET"])
+def get_users():
+
+    """#would full row for a db entry ever be required?
+    #how else to achieve other than with loop to cycle through a dict entry within a loop of returned objects?
+    # all_columns = []
+    # sample_user = User.query.first()
+    # for field, value in sample_user.__dict__.items():
+    #     all_columns.append(field)
+
+    # users = User.query.all() 
+    # users_list = []"""
+    
+    users = User.query.all()
+    #serialise the dicts by id for desired json format
+    users_list_by_id = {user.id: {'username': user.name, 'email': user.email, 'is_deleted': user.is_deleted, 'created_timestamp': user.created_timestamp} for user in users}
+    return jsonify(users = users_list_by_id)
+
+@app.route("/users/<int:id>", methods=["GET"])
+def get_user_by_id(id):
+
+    requested_user = User.query.get(id)
+    if requested_user:
+        pass
+    else:
+        return "404"
+    
+    requested_user = {'id': requested_user.id, 'username': requested_user.name, 'email': requested_user.email, "created_timestamp": requested_user.created_timestamp}
+    return jsonify(requested_user)
